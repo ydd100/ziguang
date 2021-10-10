4523fce30bf07a5787b4b328bd0bc15c379ae6b6
zhangying <550082521@qq.com>
2021-10-10 22:08:23 +0800

    测试提交

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * 启动程序123
+ * 启动程序
  * 
  * @author ruoyi
  */
